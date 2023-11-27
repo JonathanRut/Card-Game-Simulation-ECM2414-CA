04c2e3a4e7b6772ce431c195ce1ff0e5f842e8f4
JonathanRut <jmrutland@gmail.com>
2023-11-27 20:11:25 +0000

    Continued on testing

---
+++ tests/GameTestSuite.java
@@ -0,0 +1,8 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CardDeckTest.class, CardGameTest.class, CardTest.class, PlayerTest.class})
+public class GameTestSuite {
+
+}
